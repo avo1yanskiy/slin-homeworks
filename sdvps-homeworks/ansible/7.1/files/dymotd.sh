20f19a10721af371ff2ac166b5169408d7cf9d7a
Aleksandr Volyanskiy <54029550+avo1yanskiy@users.noreply.github.com>
2021-10-31 00:36:14 +0300

    Update dymotd.sh

--- sdvps-homeworks/ansible/7.1/files/dymotd.sh
+++ sdvps-homeworks/ansible/7.1/files/dymotd.sh
@@ -1,67 +1,51 @@
 #!/bin/sh
 #
-[ -r /etc/lsb-release ] && . /etc/lsb-release
- 
-if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
-        # Fall back to using the very slow lsb_release utility
-        DISTRIB_DESCRIPTION=$(lsb_release -s -d)
+# Text Color Variables http://misc.flogisoft.com/bash/tip_colors_and_formatting
+tcLtG="\033[00;37m"    # LIGHT GRAY
+tcDkG="\033[01;30m"    # DARK GRAY
+tcLtR="\033[01;31m"    # LIGHT RED
+tcLtGRN="\033[01;32m"  # LIGHT GREEN
+tcLtBL="\033[01;34m"   # LIGHT BLUE
+tcLtP="\033[01;35m"    # LIGHT PURPLE
+tcLtC="\033[01;36m"    # LIGHT CYAN
+tcW="\033[01;37m"      # WHITE
+tcRESET="\033[0m"
+tcORANGE="\033[38;5;209m"
+#
+# Time of day
+HOUR=$(date +"%H")
+if [ $HOUR -lt 12  -a $HOUR -ge 0 ]; then TIME="morning"
+elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]; then TIME="afternoon"
+else TIME="evening"
 fi
- 
-figlet $(hostname)
-printf "\n"
- 
-printf "Welcome to %s (%s).\n" "$DISTRIB_DESCRIPTION" "$(uname -r)"
-printf "\n"
- 
-#System date
-date=`date`
- 
-#System load
-LOAD1=`cat /proc/loadavg | awk {'print $1'}`
-LOAD5=`cat /proc/loadavg | awk {'print $2'}`
-LOAD15=`cat /proc/loadavg | awk {'print $3'}`
- 
-#System uptime
+#
+# System uptime
 uptime=`cat /proc/uptime | cut -f1 -d.`
 upDays=$((uptime/60/60/24))
 upHours=$((uptime/60/60%24))
 upMins=$((uptime/60%60))
-upSecs=$((uptime%60))
- 
-#Root fs info
-root_usage=`df -h / | awk '/\// {print $4}'|grep -v "^$"`
-fsperm=$(mount | grep root | awk '{print $6}' | awk -F"," '{print $1}')
- 
-#Memory Usage
-memory_usage=`free -m | awk '/Mem:/ { total=$2 } /buffers\/cache/ { used=$3 } END { printf("%3.1f%%", used/total*100)}'`
-swap_usage=`free -m | awk '/Swap/ { printf("%3.1f%%", $3/$2*100) }'`
- 
-#Users
-users=`users | wc -w`
-USER=`whoami`
- 
-#Processes
-processes=`ps aux | wc -l`
- 
-#Interfaces
-INTERFACE=$(ip -4 ad | grep 'state UP' | awk -F ":" '!/^[0-9]*: ?lo/ {print $2}')
- 
-echo "System information as of: $date"
-echo
-printf "System Load:\t%s %s %s\tSystem Uptime:\t\t%s "days" %s "hours" %s "min" %s "sec"\n" $LOAD1, $LOAD5, $LOAD15 $upDays $upHours $upMins $upSecs
-printf "Memory Usage:\t%s\t\t\tSwap Usage:\t\t%s\n" $memory_usage $swap_usage
-printf "Usage On /:\t%s\t\t\tAccess Rights on /:\t%s\n" $root_usage $fsperm
-printf "Local Users:\t%s\t\t\tWhoami:\t\t\t%s\n" $users $USER
-printf "Processes:\t%s\t\t\t\n" $processes
-printf "\n"
-printf "Interface\tMAC Address\t\tIP Address\t\n"
- 
-for x in $INTERFACE
-do
-        MAC=$(ip ad show dev $x |grep link/ether |awk '{print $2}')
-        IP=$(ip ad show dev $x |grep -v inet6 | grep inet|awk '{print $2}')
-        printf  $x"\t\t"$MAC"\t"$IP"\t\n"
- 
-done
-echo
-echo
+#
+# System + Memory
+SYS_LOADS=`cat /proc/loadavg | awk '{print $1}'`
+MEMORY_USED=`free -b | grep Mem | awk '{print $3/$2 * 100.0}'`
+SWAP_USED=`free -b | grep Swap | awk '{print $3/$2 * 100.0}'`
+NUM_PROCS=`ps aux | wc -l`
+IPADDRESS=`hostname --all-ip-addresses`
+RELEASE=`lsb_release -s -d | sed 's/"//g'`
+#
+echo -e $tcDkG "==============================================================="
+echo -e $tcLtG " Good $TIME !                                   $tcORANGE IT Dept Ltd."
+echo -e $tcDkG "==============================================================="
+echo -e $tcLtG " - Hostname      :$tcW `hostname -f`"
+echo -e $tcLtG " - IP Address    :$tcW $IPADDRESS"
+echo -e $tcLtG " - Release       :$tcW $RELEASE"
+echo -e $tcLtG " - Kernel        : `uname -a | awk '{print $1" "$3" "$12}'`"
+echo -e $tcLtG " - Users         : Currently `users | wc -w` user(s) logged on"
+echo -e $tcLtG " - Server Time   : `date`"
+echo -e $tcLtG " - System load   : $SYS_LOADS / $NUM_PROCS processes running"
+echo -e $tcLtG " - Memory used % : $MEMORY_USED"
+echo -e $tcLtG " - Swap used %   : $SWAP_USED"
+echo -e $tcLtG " - System uptime : $upDays days $upHours hours $upMins minutes"
+echo -e $tcDkG "==============================================================="
+echo -e $tcRESET ""
+#
