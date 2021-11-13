2aa7939f00a6f7a7ea6665e8bdf55e85bdbe3c71
b1t <avo1yanskiy@mail.ru>
2021-11-13 16:25:27 +0000

    aleksandr

--- sdvps-homeworks/terraform/outputs.tf
+++ sdvps-homeworks/terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "internal_ip_address_virtualmachine" {
+  value = yandex_compute_instance.virtualmachine.network_interface.0.ip_address
 }
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_virtualmachine" {
+  value = yandex_compute_instance.virtualmachine.network_interface.0.nat_ip_address
 }
 
